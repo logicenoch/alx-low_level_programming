10261cbb97ded64e036b5ed6b285643cdf326158
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-08 08:31:24 -0700

    To add c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/*Main function*/
+/**
+ *
+ ** main - main block
+ *
+ ** Return: 0
+ *
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
