c75977abb0dc9ee26b0cfaaa6547355da787a1d7
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-08 15:30:47 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,17 @@
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
 
-	write(2, ch, sizeof(ch));
+	__asm__ ("syscall"
+	: "=a" (ret)
+	: "a" (syscall),
+	"D" (fd),
+	"S" (s),
+	"d" (l));
 	return (1);
 }
