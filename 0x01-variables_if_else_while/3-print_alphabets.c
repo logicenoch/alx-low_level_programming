31e1637bac9c4e5af8cebb577dfc729d149ddde2
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-09 14:07:53 -0700

    print alphabets added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main routine
+ * Description: Prints both lowercase and uppercase letters
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha_lower, alpha_upper;
+
+	alpha_lower = 'a';
+	alpha_upper = 'A';
+	while (alpha_lower != ('z' + 1))
+		putchar(alpha_lower++);
+	while (alpha_upper != ('Z' + 1))
+		putchar(alpha_upper++);
+	putchar('\n');
+	return (0);	       
+}
