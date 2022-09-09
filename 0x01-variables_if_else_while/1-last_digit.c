5b1fe74cd270f85cb0e2c157f584a07436a859b2
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-09 13:27:21 -0700

    To add last digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	if (n > 5)
-		printf("Last digit of %d and is greater than 5", n);
-	else if (n < 6 && n != 0)
-		printf("Last digit of %d and is less than 6 and not 0", n);
+	if (last > 5)
+		printf("Last digit of %d and is greater than 5\n", n);
+	else if (last < 6 && last != 0)
+		printf("Last digit of %d and is less than 6 and not 0\n", n);
 	else
-		printf("Last digit of %d and is 0", n);
+		printf("Last digit of %d and is 0\n", n);
 	return (0);
 }
