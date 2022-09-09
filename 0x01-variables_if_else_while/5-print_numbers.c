3e5de19719de83231d92996df437612575331004
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-09 14:39:44 -0700

    Print alphabetic character excrpt q and e added!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Description: Print all numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int number = 0;
+	while (number <= 9)
+		printf("%d", number++);
+	return (0);
+}
