353af48bbcf6228cbca1d54522efc0b1c202525b
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-09 14:51:50 -0700

    Print numbers as character

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Description: Print all numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int number = '0';
+
+	while (number <= '9')
+		putchar(number++);
+	putchar('\n');
+	return (0);
+}
