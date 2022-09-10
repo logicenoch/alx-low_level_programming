7c753b65fbd0a79a0a6d632d63ce6b4581f20df2
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-09 17:28:49 -0700

    Combinaiton added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main language.
+ * Description: Print all combinations of singles - digits numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	char numeric_alpha = '0';
+
+	for (numeric_alpha = '0'; numeric_alpha <= '9'; numeric_alpha++)
+	{
+		if (numeric_alpha == '9')
+			putchar(numeric_alpha);
+		else
+		{
+			putchar(numeric_alpha);
+		}
+		putchar(',')
+	}
+	putchar('\n');
+	return (0);
+}
+
