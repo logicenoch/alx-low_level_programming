6236e23c832d6edee47f0c455d713b68e3f5e5da
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-13 17:24:00 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - custom function
+ * Description: Checks if the input is a lower case character
+ * Return: 1 if true, otherwise 0 is returned
+ */
+void _islower(int c)
+{	
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
