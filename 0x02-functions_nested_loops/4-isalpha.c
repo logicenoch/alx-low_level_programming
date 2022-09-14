83c6c3449d8487d951ce71dc83dfaf47548a76a9
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-13 17:31:50 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - custom function
+ * Description: Checks if the input is an alphabet
+ * @c: c which is a character input
+ * Return: 1 if true, otherwise 0 is returned
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
