3ca272a92322a301a1ed5110a07a236ea3572615
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-13 18:36:08 -0700

    Initial commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 void print_to_98(int n)
 {
 
-while (n <= 98)
+while (n < 98)
 {
 	printf("%i, ", n);
 	n++;
@@ -19,5 +19,6 @@ while (n > 98)
 	printf("%i, ", n);
 	n--;
 }
+printf("%d", 98);
 putchar('\n');
 }
