a77b107a4d696b190782652926590fb24693e30e
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-13 17:17:17 -0700

    Initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-	char i = 'a', j = 1;
+	char i = 'a', j = 0;
 
-	while (j <= 10)
+	while (j < 10)
 	{
 		while (i <= 'z')
 			_putchar(i++);
 	i = 'a';
+	j++;
 	_putchar('\n');
 	}
 }
