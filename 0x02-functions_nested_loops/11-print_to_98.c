1878e93cf88a59bc2835821de870981e722f12b5
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-13 18:28:27 -0700

    Initial commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * print_to_98 - print to 98
  * @n : number to start from
@@ -9,17 +9,15 @@
 void print_to_98(int n)
 {
 
-while (n < 98)
+while (n <= 98)
 {
 	printf("%i, ", n);
 	n++;
 }
-while (n > 98)
+while (n >= 98)
 {
 	printf("%i, ", n);
 	n--;
 }
-
-printf("98");
 putchar('\n');
 }
