1f2c7c71a7194d0ecdfdaebdcff847b807aa80a1
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-13 17:54:33 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - custom function
+ * Description: Returns the last digit of a number
+ * @n: contains the test data
+ * Return: last digit of @n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
