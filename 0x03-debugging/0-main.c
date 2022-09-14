a6c842f12d2c1737d7bd28f8a1b1531abc10bc97
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-14 02:57:08 -0700

    Initial commit for debugginh

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - main function
+ * Description: tests positive_or_negative function
+ * Return: 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
