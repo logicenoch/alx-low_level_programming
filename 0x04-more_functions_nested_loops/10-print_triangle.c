464833d7557854dc05e3c1f841c80e4a9a9eabca
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-15 06:53:06 -0700

    Mandatory finished

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_triangle - Prints a triangle of square according parameter
  *
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
 *function_name: main - entry point of the program
