9c1fe75262f074c304bf73c324954091f98936a7
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-15 02:03:08 -0700

    To add isdigit() function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*function_name: _isdigit - entry point of the test
+*Description: Checkes if input is a number
+*@c: Character to be tested
+*Return: 1 if successful, and 0 if unsuccessful.
+*/
+int _isupper(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
