f8589f02cfd1b2c84e08d4d38899f852b81b9621
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 03:38:26 -0700

    Fill

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *int _strlen - returns the lenghth of a string
+ *Description: takes a string and returns its length
+ *@s: the string
+ *Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(s[i] != '\0')
+		len++
+	return (len);
+}
