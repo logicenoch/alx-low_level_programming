a551e2c7e5d82214a73b8f52f692f1061db7105e
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 06:02:30 -0700

    Sring copy function added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strcpy - copy str1 to str2
+ *Description: copy string
+ *@dest:copied to here
+ *@src:copied from here
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int src_len, dest_len, counter = 0;
+
+	src_len = strlen(src);
+	dest = dtrlen(dest);
+
+	while (src[counter] != '\0')
+		dest[counter] = src[counter];
+	return dest;
+}
