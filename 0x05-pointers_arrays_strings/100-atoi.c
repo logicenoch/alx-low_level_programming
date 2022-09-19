6b194b47bae64026635f4236679415c7a73f6501
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 10:56:27 -0700

    _atoi function getting fixed up

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_atoi - converts string to integer
+ *Description: takes s as an input and converts it to an integer
+ *@s: the input string
+ *Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1, resp = 0, counter = 0, counter2;
+
+	for (counter = 0; !(s[counter] >= 48 && s[counter] <= 57); counter++)
+		sign = (s[counter] == '-') ? -1 : 1;
+	for (counter2 = counter; s[counter2] >= 48 && s[counter2] <= 57; counter2++)
+		resp = (resp * 10) + (s[counter2] - '0');
+	return (sign * resp);
+}
