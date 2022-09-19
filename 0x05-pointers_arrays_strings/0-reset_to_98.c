765cb35d4e36453be5812ffede299f83f15bb406
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 03:19:13 -0700

    Fill

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *reset_to_98 - updates poiinter:
- *Description:  a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ *Description: updates the value it points to to 98.
  *@n: input pointer
  *Return: void
  */
