de6525bcd0fce1563ffcb66e1a556b24852d83af
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 11:09:59 -0700

    _atoi function getting fixed up

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,11 +7,17 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, counter = 0, counter2;
+	int sign = 1, result = 0;
 
-	for (counter = 0; !(s[counter] >= 48 && s[counter] <= 57); counter++)
-		sign *= (s[counter] == '-') ? -1 : 1;
-	for (counter2 = counter; s[counter2] >= 48 && s[counter2] <= 57; counter2++)
-		resp = resp * 10 + s[counter2] - '0';
-	return (sign * resp);
+	while (s[0] != '\0')
+	{
+		if (s[0] == '-')
+			sign *= -1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0') * sign;
+		else if (result)
+			break;
+		s++;
+	}
+	return (result);
 }
