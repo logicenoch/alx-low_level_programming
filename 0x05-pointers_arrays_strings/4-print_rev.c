8b72ac17b767f69fdbd9d32bbf0dae1a6b1e3f06
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 04:07:44 -0700

    Hellofile added!

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_rev - prints the reverse of a string
+ *Description: Resverses its input
+ *@str:input string
+ *Return: void
+ */
+void print_rev(char *str)
+{
+	int len = strlen(str) - 1;
+
+	while (len-- >= 0)
+		_putchar(str[len]);
+}
