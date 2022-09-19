2d9160a5e0eed1951227843390fac6597d3eea17
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-19 10:33:59 -0700

    Getting file ready for fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,10 @@ char *_strcpy(char *dest, char *src)
 	int counter = 0;
 
 	while (src[counter] != '\0')
+	{
 		dest[counter] = src[counter];
+		counter++;
+	}
 	dest[counter] = '\0';
 	return (dest);
 }
