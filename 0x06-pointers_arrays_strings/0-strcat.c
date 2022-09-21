95e1aed302563b6eb397a6aa7e22a52ff9f17d79
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-21 10:43:39 -0700

    File name pushed

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @src: source string
+ * @dest: destination or output string
+ * Return: dest goes out here
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, len = strlen(dest++);
+
+	for (a = 0; dest[len] != '\0'; a++)
+	{
+		dest[len] = src[a];
+		len++;
+	}
+
+	return (dest);
+}
