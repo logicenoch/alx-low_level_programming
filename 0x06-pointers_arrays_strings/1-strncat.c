e0caec9a47c1f01a54df01893906cba490af8c85
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-21 10:12:54 -0700

    File uploaded

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _strncat - concatenates two strings
+ * @n: number of bytes from source
+ * @src: source string
+ * @dest: destination or output string
+ * Return: dest goes out here 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	dest += src;
+	return dest;
+}
