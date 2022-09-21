7bef684a3341fa0873212502aae242b665cb6461
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-21 10:22:25 -0700

    README.md added

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _strncat - concatenates two strings
+ * _strcat - concatenates two strings
  * @n: number of bytes from source
  * @src: source string
  * @dest: destination or output string
- * Return: dest goes out here 
+ * Return: dest goes out here
  */
 
 char *_strcat(char *dest, char *src)
 {
 	dest += src;
-	return dest;
+	return (dest);
 }
