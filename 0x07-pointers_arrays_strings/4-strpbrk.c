fd31a1417cda0118d127630214b01a7d9b16f8cb
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-26 09:02:35 -0700

    fixing strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,12 +10,12 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j;
 	char *match;
 
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
