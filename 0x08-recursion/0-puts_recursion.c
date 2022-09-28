107163676aee94dd58295834e65a24cca2ca8918
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-28 05:15:22 -0700

    Initial commit to program

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ *_puts_recursion - prints a string using recursion technique
+ *@s: a pointer to char or string to print
+ *Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursive(s + 1);
+	}
+}
