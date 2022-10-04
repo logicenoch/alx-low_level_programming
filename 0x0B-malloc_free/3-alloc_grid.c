f41fdf519bfda575f28655ed4ba6c8f05ca8d140
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-04 01:35:59 +0100

    pushed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	
-	grid = mallac(grid_size * sizeof(int));
+	grid = malloc(grid_size * sizeof(int));
 
 	if (grid == NULL)
 		return (NULL);
