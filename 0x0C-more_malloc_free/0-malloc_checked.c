6f0dfa50fcf68e9f42ab789c1d70dd3496b43661
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-06 00:34:14 -0700

    Initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *malloc_checked - Allocates memory using pointer
+ *@b: input
+ *Return: a pointer or 98 on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof *ptr);
+
+	if (ptr == NULL)
+		return (98);
+	*ptr = b;
+
+	return (b);
+}
