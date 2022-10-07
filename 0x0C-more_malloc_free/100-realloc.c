767744848e2f6352653febe9bd3465e9a78919b5
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-07 06:21:48 -0700

    fixed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *array_range - reallocates a memory block
+ *_realloc - reallocates a memory block
  *@ptr: pointer to the memory previously allocated with a call to malloc.
  *@old_size: size, in bytes, of the allocated.
  *@new_size: the new size, in bytes of the new memory block.
  *Return: pointer to the array output
  */
 
- void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
- {
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
 	void *extend;
+	int i;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -19,7 +20,7 @@
 		extend = malloc(new_size);
 		if (extend == NULL)
 			return (NULL);
-		return (extend);	
+		return (extend);
 	}
 
 	if (new_size == 0 && ptr)
@@ -28,16 +29,15 @@
 		return (NULL);
 	}
 
-
 	extend = malloc(new_size);
 
 	if (extend == NULL)
 		return (NULL);
-		
+
 	for (i = 0; i < old_size; i++)
 		*((char *)extend + i) = *((char *)extend + i);
 
 	free(ptr);
 
 	return ((void *) extend);
- }
+}
