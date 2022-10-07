bffe77dbb9bcc2a929c115db08d603fc6eb70b99
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-07 13:57:21 +0100

    fixed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max) 
 		return (NULL);
 
-	arr = malloc(sizeof(int) * len + 1);
+	arr = malloc(sizeof(int) * (len + 1));
 
 	if (arr == NULL)
 		return (NULL);
