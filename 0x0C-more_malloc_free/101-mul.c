01576842cd36a84c0762d87adef935b65c43861a
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-07 06:32:42 -0700

    fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,31 +1,91 @@
 #include "main.h"
+
 /**
- * main - Houses a program that multiplies through CML
- *@argc: argument count
- *@argv: arrays of arguments
- *Return: 0 if successful, -1 otherwise
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+	_putchar(str[i]);
+	i++;
+}
+
+}
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
 
-int main(int argc, char **argv)
+int _atoi(const char *s)
 {
-	int num1, num2;
+    int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
 
-	if (argc != 3)
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
-		printf("Error\n");
-		exit(98);
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
 	}
 
-	if (atoi(argv[1]) && atoi(argv[2]))
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
 
-	}
-	else
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	return (num1 * num2);
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
+ */
+
+void print_int(unsigned long int n)
+{
+
+unsigned  long int divisor = 1, i, resp;
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+	resp = n / divisor;
+	_putchar('0' + resp);
+}
+
+}
+
+/**
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+
+if (argc != 3)
+{
+	_puts("Error ");
+	exit(98);
+}
+print_int(_atoi(argv[1]) * _atoi(argv[2]));
+_putchar('\n');
+
+return (0);
 }
