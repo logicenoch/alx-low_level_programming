570590ec74380bfad3f4cc306aa7477f55479ff2
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-10 21:39:22 +0100

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ *struct dog - a structure that defines a dog.
+ *@name: holds the name of the dog.
+ *@age: holds the age of the dog.
+ *@owner: 
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
