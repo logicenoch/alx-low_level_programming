14f7e24bd57771de4ac1a0ddeb46cdc375f95c55
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-10 23:07:42 +0100

    To add file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	new_dog->name = malloc(sizeof(char) * strlen(name));
 
 	if (new_dog->name == NULL)
 	{
@@ -30,7 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog->age = age;
 
-	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	new_dog->owner = malloc(sizeof(char) * (strlen(owner));
 
 	if (new_dog->owner == NULL)
 	{
