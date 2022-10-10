f593a4404aa3ad9fbb336545e9f094167a3ccdc5
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-10 16:11:45 +0100

    final

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef ABS_DEFINITION
 #define ABS_DEFINITION
-#define ABS(x) ((x > 0)? (x) : (x) * -1)
+#define ABS(x) ((x > 0)? (x) : ((x) * -1))
 #endif /*end ABS_DEFINITION*/
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,4 @@
 #ifndef SUM_DEFINITION
 #define SUM_DEFINITION
-#define SUM(x, y) (x) + (y)
+#define SUM(x, y) ((x) + (y))
 #endif
