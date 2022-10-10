c7c7b19c23be3f1173addef3f854f9af358b7b26
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-10 16:13:09 +0100

    final commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef ABS_DEFINITION
-#define ABS_DEFINITION
-#define ABS(x) ((x > 0)? (x) : ((x) * -1))
+#ifndef ABS_H
+#define ABS_H
+#define ABS(x) (x > 0? (x) : ((x) * -1))
 #endif /*end ABS_DEFINITION*/
