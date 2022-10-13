303a0d716c3c86261d9138f9e751fd141ea6ee99
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-13 09:52:28 -0700

    searcher

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - searches for an integer
+ *@size: size of an array.
+ *@array: array to be searched.
+ *@cmp: pointer the function cmp().
+ *Return: index of the first element found.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (size <= 0)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]))
+			return (index);
+	}
+	return (-1);
+}
