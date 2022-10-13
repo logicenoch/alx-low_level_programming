35e995888cbfbaa99f20dbd1e8d66294a02642cd
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-13 09:02:32 -0700

    Initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ *print_name - prints the name passed to the function.
+ *@name: the name passed to the function.
+ *@f: a pointer to a function.
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
