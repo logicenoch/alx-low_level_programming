b9ca2962c95bb2e7c6d2d6b5f83cc046bc76ded6
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-13 09:33:55 -0700

    Initial commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+/**
+ *array_iterator - executes a function passed to it
+ *@array: array passed as an arg
+ *@size: size of the array
+ *@action: pointer to the function to be executed
+ *Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int counter = 0;
+
+	if (action == NULL || array == NULL)
+		return;
+	for (counter = 0; counter < size; counter++)
+	{
+		(*action)(array[i]);
+	}
+}
